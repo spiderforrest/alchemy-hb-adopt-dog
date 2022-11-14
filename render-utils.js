a36f2e5892504d98771e9eb14cb3da455829e063
spiderforrest <spider@spood.org>
2022-11-14 15:00:13 -0800

    dogs r clickable

--- render-utils.js
+++ render-utils.js
@@ -8,7 +8,7 @@ export function renderDogCard(dog) {
 
     p.textContent = dog.name;
     img.src = `./assets/${dog.breed}.jpeg`;
-    a.href = ''; // link to the dog's detail page here
+    a.href = `./detail/?id=${dog.id}`; // link to the dog's detail page here
 
     div.append(p, img);
 
