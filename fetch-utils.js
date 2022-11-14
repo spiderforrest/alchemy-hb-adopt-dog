9729f71714c98decdab6f771d0c0f753219b88ac
spiderforrest <spider@spood.org>
2022-11-14 15:04:40 -0800

    return a dog

--- fetch-utils.js
+++ fetch-utils.js
@@ -13,5 +13,7 @@ export async function getDogs() {
 
 export async function getDog(id) {
     // from the dogs table, select a single dog who has the matching id
+    const response = await client.from('dogs').select().match({ id: id }).single();
     // and return the response
+    return response;
 }
